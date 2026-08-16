979aec53d3814600af1149bc3d9014b2b69dfa27
Raumaniga <duyp51739@gmail.com>
2026-08-16 16:17:18 +0700

    Fix YOLO AutoShape AMP inference import

--- models/common.py
+++ models/common.py
@@ -23,7 +23,7 @@ import torch
 import torch.nn as nn
 from IPython.display import display
 from PIL import Image
-#from torch.cuda import amp
+from torch.cuda import amp
 
 from utils import TryExcept
 from utils.dataloaders import exif_transpose, letterbox
